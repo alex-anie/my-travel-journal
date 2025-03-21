230d9981070c09841b4e82a922126b63603d76ac
alex-anie <ocxigin@gmail.com>
2025-03-21 19:09:35 +0100

    Goodluck Jonathan Wikipedia page added for testing sake

---
+++ cypress/e2e/wiki.cy.js
@@ -0,0 +1,8 @@
+describe('Load Wikipedia of Goodluck Jonathan', () => {
+    it('visit the wiki url', () => {
+      cy.visit('https://en.wikipedia.org/wiki/Goodluck_Jonathan')
+    })
+  })
+  
+  
+
