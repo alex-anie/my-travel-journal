46d4fce54a474e8c27acaff26b13872a8d9655a1
alex-anie <ocxigin@gmail.com>
2025-03-21 16:27:44 +0100

    Cypress is added

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export default [
     endDate: "29th Feb, 2021",
     description:
       "Venice, the capital of northern Italy’s Veneto region, is built on more than 100 small islands in a lagoon in the Adriatic Sea. It has no roads, just canals including the Grand Canal thoroughfare lined with Renaissance and Gothic palaces. The central square, Piazza San Marco, contains St. Mark’s Basilica, which is tiled with Byzantine mosaics, and the Campanile bell tower offering views of the city’s red roofs",
-    imageUrl: "https://source.unsplash.com/07SuOaLjuTY",
+    imageUrl: "https://plus.unsplash.com/premium_photo-1677613926650-a327866c9da2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dmVuaWN8ZW58MHx8MHx8fDA%3D",
   },
 
   {
@@ -20,7 +20,7 @@ export default [
     endDate: "29th Mar, 2021",
     description:
       "Antarctica, the southernmost continent and site of the South Pole, is a virtually uninhabited, ice-covered landmass. Most cruises to the continent visit the Antarctic Peninsula, which stretches toward South America. It’s known for the Lemaire Channel and Paradise Harbor, striking, iceberg-flanked passageways, and Port Lockroy, a former British research station turned museum. The peninsula’s isolated terrain also shelters rich wildlife, including many penguins",
-    imageUrl: "https://source.unsplash.com/Y-nyDv3TWm0",
+    imageUrl: "https://images.unsplash.com/photo-1568039884582-ae3c5f59e6f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmVuaWN8ZW58MHx8MHx8fDA%3D",
   },
 
   {
@@ -32,7 +32,7 @@ export default [
     endDate: "29th Apr, 2021",
     description:
       "Greece is a country in southeastern Europe with thousands of islands throughout the Aegean and Ionian seas. Influential in ancient times, it's often called the cradle of Western civilization. Athens, its capital, retains landmarks including the 5th-century B.C. Acropolis citadel with the Parthenon temple. Greece is also known for its beaches, from the black sands of Santorini to the party resorts of Mykonos.",
-    imageUrl: "https://source.unsplash.com/XGKaRnWjv1c",
+    imageUrl: "https://images.unsplash.com/photo-1566404791232-af9fe0ae8f8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVybGlufGVufDB8fDB8fHww",
   },
 
   {
@@ -44,7 +44,7 @@ export default [
     endDate: "29th May, 2021",
     description:
       "Peru is a country in South America that's home to a section of Amazon rainforest and Machu Picchu, an ancient Incan city high in the Andes mountains. The region around Machu Picchu, including the Sacred Valley, Inca Trail and colonial city of Cusco, is rich in archaeological sites. On Peru’s arid Pacific coast is Lima, the capital, with a preserved colonial center and important collections of pre-Columbian art.",
-    imageUrl: "https://source.unsplash.com/t0Gj3WWBr8I",
+    imageUrl: "https://images.unsplash.com/photo-1622182438074-b163bdf8f16c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJlcmxpbnxlbnwwfHwwfHx8MA%3D%3D",
   },
 
   {
@@ -56,7 +56,7 @@ export default [
     endDate: "29th June, 2021",
     description:
       "The Maldives, officially the Republic of Maldives, is an archipelagic state and country in South Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland.",
-    imageUrl: "https://source.unsplash.com/DtWyp_4YEes",
+    imageUrl: "https://images.unsplash.com/photo-1597932552386-ad91621e4c8a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJlcmxpbnxlbnwwfHwwfHx8MA%3D%3D",
   },
 
   {
@@ -68,7 +68,7 @@ export default [
     endDate: "29th July, 2021",
     description:
       "New Zealand is an island country in the southwestern Pacific Ocean. It consists of two main landmasses—the North Island and the South Island —and over 700 smaller islands. It is the sixth-largest island country by area, covering 268,021 square kilometres",
-    imageUrl: "https://source.unsplash.com/6ajf6BAyYt4",
+    imageUrl: "https://images.unsplash.com/photo-1573380499492-185441433ccd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
@@ -80,7 +80,7 @@ export default [
     endDate: "29th Aug, 2021",
     description:
       "The U.S. is a country of 50 states covering a vast swath of North America, with Alaska in the northwest and Hawaii extending the nation’s presence into the Pacific Ocean. Major Atlantic Coast cities are New York, a global finance and culture center, and capital Washington, DC. Midwestern metropolis Chicago is known for influential architecture and on the west coast, Los Angeles' Hollywood is famed for filmmaking",
-    imageUrl: "https://source.unsplash.com/jNUTiYlPMHI",
+    imageUrl: "https://images.unsplash.com/photo-1605978208410-c3deb0fab40d?q=80&w=1438&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
@@ -92,7 +92,7 @@ export default [
     endDate: "29th Sep, 2021",
     description:
       "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré",
-    imageUrl: "https://source.unsplash.com/Q0-fOL2nqZc",
+    imageUrl: "https://images.unsplash.com/photo-1549144511-f099e773c147?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
@@ -104,7 +104,7 @@ export default [
     endDate: "29th Oct, 2021",
     description:
       "Iceland, a Nordic island nation, is defined by its dramatic landscape with volcanoes, geysers, hot springs and lava fields. Massive glaciers are protected in Vatnajökull and Snæfellsjökull national parks. Most of the population lives in the capital, Reykjavik, which runs on geothermal power and is home to the National and Saga museums, tracing Iceland’s Viking history.",
-    imageUrl: "https://source.unsplash.com/36K5WckeU3o",
+    imageUrl: "https://images.unsplash.com/photo-1610143987332-e8c83dada7fc?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
@@ -116,6 +116,6 @@ export default [
     endDate: "29th Nov, 2021",
     description:
       "Germany is a Western European country with a landscape of forests, rivers, mountain ranges and North Sea beaches. It has over 2 millennia of history. Berlin, its capital, is home to art and nightlife scenes, the Brandenburg Gate and many sites relating to WWII. Munich is known for its Oktoberfest and beer halls, including the 16th-century Hofbräuhaus. Frankfurt, with its skyscrapers, houses the European Central Bank",
-    imageUrl: "https://source.unsplash.com/4xFUoiW3i4k",
+    imageUrl: "https://images.unsplash.com/photo-1566404791232-af9fe0ae8f8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVybGlufGVufDB8fDB8fHww",
   },
 ];
